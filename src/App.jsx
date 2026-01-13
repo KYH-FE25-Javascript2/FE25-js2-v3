aa48182b3a5ed3eabbd30f9c3b8db642d6fb2d41
Mandus Lindström <mandus85@hotmail.com>
2026-01-13 12:01:21 +0100

    vite and react boiler plate installation, done with code along

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+let hej = "Hej igen Klassen!";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Hej Klassensdffsdadsfafddsaf!</p>
+      <p>{hej}</p>
+    </>
+  );
+}
+
+export default App;
